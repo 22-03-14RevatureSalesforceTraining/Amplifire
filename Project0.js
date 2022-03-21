aba795ad962f5615d5f11b2835775ade20733fcd
adambaird8 <adamkurtbaird@gmail.com>
2022-03-21 10:51:08 -0400

    Text box functionality

--- Project0.js
+++ Project0.js
@@ -1,9 +1,11 @@
 const char_pic = document.getElementById('char_pic');
 const char_info = document.getElementById('char_info');
+const text = document.getElementById('name');
 const submit = document.getElementById('submit');
 const random = document.getElementById('random');
 var gryfChars, slytChars, huffChars, raveChars, characters = new Array();
 
+text.addEventListener('keypress', userInput);
 submit.addEventListener('click', userInput);
 random.addEventListener('click', randomChar);
 
@@ -21,10 +23,10 @@ async function fetchChars() {
 }
 
 
-window.onload = (event) => { //make Harry Potter the default character when page is loaded
+window.onload = (event) => {
     fetchChars().then(characters => {
-        getChar("Harry Potter");
-        createTable();
+        getChar("Harry Potter"); //make Harry Potter the default character when page is loaded
+        createTable(); //populate table with characters when page loaded
     })
 };
 
@@ -73,7 +75,7 @@ function createTable() {
 
 
 function userInput() {
-    const input = document.getElementById('name');
+    input = document.getElementById('name').value;
     getChar(input);
 }
 
@@ -101,9 +103,9 @@ function getChar(name) {
             char_info.style = "color: rgb(26, 71, 42);";
             break;
         case 'Hufflepuff':
-            char_name.innerHTML = '<p style="color: rgb(255, 216, 0);">'+characters[charPos].name+'</p>';
+            char_name.innerHTML = '<p style="color: rgb(238, 198, 0);">'+characters[charPos].name+'</p>';
             char_pic.innerHTML = '<img src="hufflepuff.jpg"/>';
-            char_info.style = "color: rgb(255, 216, 0);";
+            char_info.style = "color: rgb(255, 221, 0);";
             break;
         case 'Ravenclaw':
             char_name.innerHTML = '<p style="color: rgb(14, 26, 64);">'+characters[charPos].name+'</p>';
@@ -115,17 +117,28 @@ function getChar(name) {
     if(characters[charPos].image)
         char_pic.innerHTML = '<img src=\"'+characters[charPos].image+'"/>';
 
-    var detail;
+    var result, finalResult, val, detail;
     char_info.innerHTML = ''; //reset the list
+
     for (const key in characters[charPos]) {
-        if(key != "wand" && key != "image" && key != "name" && `${characters[charPos][key]}`)
-        {
+        if (key != "name" && key != "wand" && key != "image" && `${characters[charPos][key]}`) {
+            result = key.replace( /([A-Z])/g, " $1" );
+            finalResult = result.charAt(0).toUpperCase() + result.slice(1);
+            console.log(characters[charPos][key]);
+
+            if(characters[charPos][key] == true)
+                val = "yes";
+            else if(characters[charPos][key] == false)
+                val = "no";
+            else
+                val = characters[charPos][key];
             detail = document.createElement('li');
-            detail.appendChild(document.createTextNode(`${key}: ${characters[charPos][key]}`));
+            detail.appendChild(document.createTextNode(`${finalResult}: ${val}`));
             char_info.appendChild(detail);
         }
     }
 }
+
 
 function randomChar() {
     var randomNum = Math.floor(Math.random() * characters.length);

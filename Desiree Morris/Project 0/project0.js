54ec73f6f8b0e201f6cfc5a90d6bb066cc8bafc9
Desiree Morris <amethystdee@hotmail.com>
2022-03-21 18:16:02 -0400

    scaffold out HTML highlighted layout with CSS and planned JS

--- Desiree Morris/Project 0/project0.js
+++ Desiree Morris/Project 0/project0.js
@@ -3,3 +3,38 @@ function helloWorld(){
 };
 
 helloWorld();
+
+/* Navigation Bar: When clicking the Villian/Hero button in the navigation
+change the style of the page.
+change the type of characters generated from good to evil.
+change the new button to say either New Villian or New Hero. 
+make sure any text is updated to say either villian or hero as well*/
+
+
+
+/* Random Generator Section: on load include a picture that says
+"click the new hero/villian button to generate a random hero/villian" */
+
+
+
+/* When clicking the new button make an AJAX request to the hero API
+to return just one hero or villian (may need a filter feature and loop to return just a hero
+or just a villian) back and display a card with their infomation
+also handle errors here*/
+
+
+
+/* Choose Your Team Section: on load include a picture that says 
+"click either button to choose your team and generate a list of marvel or dc comic characters"*/
+
+
+
+/*When clicking the Marvel button make an AJAX request to the hero API to return
+a list of heros or villians and filter them to only be Marval, then display them to
+the page with their cards*/
+
+
+
+/*When clicking the Dc Comics button make an AJAX request to the hero API to return
+a list of heros or villians and filter them to only be DC Comics, then display them to
+the page with their cards*/

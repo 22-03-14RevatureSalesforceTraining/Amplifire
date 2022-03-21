aef93fd08ae246d050d91ef04bdf4c21e8666272
sampreston11 <s4preston0@gmail.com>
2022-03-21 10:57:20 -0400

    Began Project0

---
+++ sampreston11/SamPreston_Project0.js
@@ -0,0 +1,9 @@
+/*
+SamPreston_Project0
+Sam Preston
+Created Date: 3/18/22
+Last Modiifed Date: 3/20/22
+This file includes the JavaScript methods that are used to satisfy the requiremenets of Project0
+,
+*/ 
+

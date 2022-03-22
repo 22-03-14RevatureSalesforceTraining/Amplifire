b31a4a4e4afc10638f942ea7146df7200133fda1
Austin McElrone <a.c.mcelrone@gmail.com>
2022-03-21 18:09:58 -0700

    final commit

--- Austin_Project0/poject0McElrone.js
+++ Austin_Project0/poject0McElrone.js
@@ -1,8 +1,15 @@
+
+/*fileName: project0McElrone.js
+  Author: Austin McElrone
+  created:3/18/22
+  last modified:3/21/22
+  Overview: This file provides the user interactivity for html file project0McElrone.html*/
+
 let country;
 let amount;
 let loopBreak = false;
 
-document.getElementById('startButton').addEventListener('click', buttonClicked);
+document.getElementById('startButton').addEventListener('click', buttonClicked); // Event listener
 
 function buttonClicked(){
     try{
@@ -19,35 +26,35 @@ function buttonClicked(){
 
         console.log(amount);
 
-    while(loopBreak != true ){
-        if(country == 'England'){
-        document.getElementById("flag").src = "images/England.png";
-        fetch('https://v6.exchangerate-api.com/v6/ae52137dc15cbfa6d0bf5c37/latest/USD')
-        .then(response => response.json())
-        .then(data => {console.log(data);
+        while(loopBreak != true ){//if true, loop will break
+            if(country == 'England'){
+                document.getElementById("flag").src = "images/England.png";
+                fetch('https://v6.exchangerate-api.com/v6/ae52137dc15cbfa6d0bf5c37/latest/USD')
+                .then(response => response.json())
+                .then(data => {console.log(data);
                 console.log(data.conversion_rates.GBP);
                 document.getElementById("conversionRate").innerHTML = data.conversion_rates.GBP;
                 document.getElementById("conversion").innerHTML = data.conversion_rates.GBP*amount;
-            });
+                });
 
-        loopBreak = true;
-    }
-    else if(country == 'United Arab Emirates'){
-        document.getElementById("flag").src = "images/UAE.png";
-        fetch('https://v6.exchangerate-api.com/v6/ae52137dc15cbfa6d0bf5c37/latest/USD')
-        .then(response => response.json())
-        .then(data => {console.log(data);
+                loopBreak = true;
+            }
+            else if(country == 'United Arab Emirates'){
+                document.getElementById("flag").src = "images/UAE.png";
+                fetch('https://v6.exchangerate-api.com/v6/ae52137dc15cbfa6d0bf5c37/latest/USD')
+                .then(response => response.json())
+                .then(data => {console.log(data);
                 console.log(data.conversion_rates);
                 document.getElementById("conversionRate").innerHTML = data.conversion_rates.AED;
                 document.getElementById("conversion").innerHTML = data.conversion_rates.AED*amount;
             }); 
             loopBreak = true;  
-    }
-    else if(country == 'Japan'){
-        document.getElementById("flag").src = "images/Japan.png";
-        fetch('https://v6.exchangerate-api.com/v6/ae52137dc15cbfa6d0bf5c37/latest/USD')
-        .then(response => response.json())
-        .then(data => {console.log(data);
+        }
+        else if(country == 'Japan'){
+            document.getElementById("flag").src = "images/Japan.png";
+            fetch('https://v6.exchangerate-api.com/v6/ae52137dc15cbfa6d0bf5c37/latest/USD')
+            .then(response => response.json())
+            .then(data => {console.log(data);
                 console.log(data.conversion_rates.JPY);
                 document.getElementById("conversionRate").innerHTML = data.conversion_rates.JPY;
                 document.getElementById("conversion").innerHTML = data.conversion_rates.JPY*amount;
@@ -57,26 +64,26 @@ function buttonClicked(){
             });
 
             loopBreak = true;
-    }
-    else if(country == 'France'){
-        document.getElementById("flag").src = "images/France.png";
-        fetch('https://v6.exchangerate-api.com/v6/ae52137dc15cbfa6d0bf5c37/latest/USD')
-        .then(response => response.json())
-        .then(data => {console.log(data);
+        }
+        else if(country == 'France'){
+            document.getElementById("flag").src = "images/France.png";
+            fetch('https://v6.exchangerate-api.com/v6/ae52137dc15cbfa6d0bf5c37/latest/USD')
+            .then(response => response.json())
+            .then(data => {console.log(data);
                 console.log(data.conversion_rates.EUR);
                 document.getElementById("conversionRate").innerHTML = data.conversion_rates.EUR;
                 document.getElementById("conversion").innerHTML = data.conversion_rates.EUR*amount;
             });
 
             loopBreak = true;
-    }
-    else{
+        }
+        else{
         country = prompt("Enter a country:");
-    }
+        }
     }
 }
-catch(error){
+    catch(error){
     console.log(error);
-}
+    }
 }
 

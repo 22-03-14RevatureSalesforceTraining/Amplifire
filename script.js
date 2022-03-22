43d5644d0188875f394755dff5bb107d58f20d38
nthquan35 <hongquan03051997@gmail.com>
2022-03-22 06:59:24 -0700

    final vers

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const button = document.getElementById("generatorButton");
 const userInput = document.getElementById("userInput");
 const main = document.getElementById("insertion");
+const errorMessage = document.getElementById("errorMessage");
+const arr = [];
 
 function checkInputLength(){
 	return userInput.value.length;
@@ -10,23 +12,55 @@ const generateRobot = async () => {
 	try{
 		if (checkInputLength() == 0)
 			throw new Error("Empty Input!");
-		const url = `https://robohash.org/${userInput.value}`;
-		const figure = document.createElement("figure");
-		const image = document.createElement("img");
-		const name = document.createElement("p");
-		image.src =  url;
-		name.appendChild(document.createTextNode(userInput.value));
-		figure.appendChild(image);
-		figure.appendChild(name);
-		main.appendChild(figure);
-		// console.log(userInput.value);
-		// console.log(url);
+
+		const object = {
+			name : userInput.value,
+			url: `https://robohash.org/${userInput.value}`
+		}
+		arr.push(object);
+		displayRobot(arr);
 		userInput.value = "";
 
 	}
 	catch (err){
-		console.log(err);
+		if (err.message === "Empty Input!"){
+			errorMessage.innerHTML += "No Input Found!";
+			setTimeout(() => {
+				errorMessage.innerHTML = "";
+			}, 3000);
+		}
+		else
+			console.log(err);
 	}
+}
+
+function displayRobot(arr){
+	main.innerHTML = "";
+	for (let i = 0; i < arr.length; i++){
+		const url = arr[i].url;
+		const div = document.createElement("div");
+		const xButton = document.createElement("button");
+		const image = document.createElement("img");
+		const name = document.createElement("a");
+		image.src =  url;
+
+		name.href = url;
+		name.appendChild(document.createTextNode(arr[i].name));
+
+		xButton.className = "x";
+		xButton.appendChild(document.createTextNode("X"));
+		xButton.setAttribute('onclick',`deleteButtonClicked()`);
+		
+		div.appendChild(xButton);
+		div.appendChild(image);
+		div.appendChild(name);
+
+		main.appendChild(div);
+	}
+}
+
+function deleteButtonClicked(event){
+	console.log(event);
 }
 
 function enterKeyPressed(event){

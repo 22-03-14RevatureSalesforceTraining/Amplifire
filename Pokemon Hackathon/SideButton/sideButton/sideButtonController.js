b38fda8b3271b310d8b1fffa44cf431db29fcd51
sampreston11 <s4preston0@gmail.com>
2022-04-28 14:58:24 -0400

    anything

---
+++ Pokemon Hackathon/SideButton/sideButton/sideButtonController.js
@@ -0,0 +1,13 @@
+({
+    leftClick : function(component, event, helper) {
+        let component = leftCLick(event);
+        
+    }
+})
+
+({
+    rightClick : function(component, event, helper) {
+        let component = rightClick(event);
+
+    }
+})

5df451f38389fbb9c6990cce97aaf192fd173286
sampreston11 <s4preston0@gmail.com>
2022-04-28 14:47:33 -0400

    anything

--- sampreston11/SamPreston_Project0.js
+++ sampreston11/SamPreston_Project0.js
@@ -4,6 +4,17 @@ Sam Preston
 Created Date: 3/18/22
 Last Modiifed Date: 3/20/22
 This file includes the JavaScript methods that are used to satisfy the requiremenets of Project0
-,
 */ 
 
+
+
+function searchPlayer(){
+    let input = document.getElementById(message);
+    document.querySelector(input).scrollIntoView();
+    
+    
+    //window.location.href="#userInput";
+    
+    
+    
+}
